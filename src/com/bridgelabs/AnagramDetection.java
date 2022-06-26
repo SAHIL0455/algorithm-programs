fdada82a5aedd5e2e2c81d5a563606868c28d518
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-26 21:30:52 +0530

    ADDED: check string is anagram or not

---
+++ src/com/bridgelabs/AnagramDetection.java
@@ -0,0 +1,20 @@
+package com.bridgelabs;
+
+import java.util.*;
+
+public class AnagramDetection {
+    public static void main(String[] args) {
+        String word1 = "heart";
+        String word2 = "earthf";
+        char arr1[] = word1.toCharArray();
+        char arr2[] = word2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        boolean result = Arrays.equals(arr1,arr2);
+        if (result == true) {
+            System.out.println("This is the Anagram");
+        } else {
+            System.out.println("This is Not Anagram");
+        }
+    }
+}
